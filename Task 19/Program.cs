2d678cd5267cab2899ec7970067755e5109c78aa
Pavel <p.a.dudarev@mail.ru>
2022-09-08 08:50:20 +0500

    Task 19

---
+++ Task 19/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите пятизначное число: ");
+string number = Console.ReadLine();
+int count = 0;
+
+for (int i = 0; i < number.Length / 2; i++)
+{
+    if(number[i] == number[number.Length - i - 1])
+    {
+       count++;
+    }
+
+
+}
+if (count == number.Length/2)
+{
+    Console.WriteLine($"Число {number} является палиндромом");
+}
+else
+{
+    Console.WriteLine("Число не является палинромом");
+}
