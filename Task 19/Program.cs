099076969daa0b3d3302236a4aef224a419b8672
Pavel <p.a.dudarev@mail.ru>
2022-09-10 14:58:14 +0500

    Добавил проверку на число

--- Task 19/Program.cs
+++ Task 19/Program.cs
@@ -1,5 +1,17 @@
 ﻿Console.WriteLine("Введите пятизначное число: ");
 string number = Console.ReadLine();
+bool check = int.TryParse(number,out var number1);
+if(check == false)
+{
+    while(check == false)
+   {
+     Console.WriteLine("Вы ввели не число, введите, пятизначное число: ");
+     number = Console.ReadLine();
+     check = int.TryParse(number,out number1);
+   }
+
+}
+
 int count = 0;
 
 for (int i = 0; i < number.Length / 2; i++)
